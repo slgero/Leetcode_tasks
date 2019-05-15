a936fe8f3d71beb7cd1b1183f436011c3c23a0d0
SERGEY SAVVOV <sersavvov@yandex.ru>
2019-05-15 14:34:36 +0300

    Create solution.cpp

---
+++ Two_Sum/solution.cpp
@@ -0,0 +1,38 @@
+#include <unordered_map>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> v;
+        for(int i = 0; i < nums.size(); ++i){
+            m.insert(make_pair(nums[i], i));  // It's faster than m[nums[i]] = i
+        }
+        for(int i = 0; i < nums.size(); ++i){
+            int dif = target - nums[i];
+            if (m.count(dif) == 1){
+                if (m[dif] != i)
+                    return {i, m[dif]};
+            }
+        }
+        return v;
+    }
+};
+
+int main(){
+    // Tests:
+    vector<int> v = {1,8,2,9,4,0,5,2};  // 7 -> [2, 6]
+    vector<int> v1 = {-1,-2,-3,-4,-5};  // -8 -> [2,4]
+    vector<int> v2 = {0, 4, 3, 0};      //0 -> [0,3]
+    Solution sol;
+
+    for (const auto& x : sol.twoSum(v2, 0)){
+        cout << x << " ";
+    }
+    return 0;
+}
